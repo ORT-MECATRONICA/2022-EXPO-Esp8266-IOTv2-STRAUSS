8b962ec2907eacb8a445c0867b29586329b557dc
Kippur <marcos.strauss.ort@gmail.com>
2021-09-12 18:38:25 -0300

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,14 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
+#include <DHT.h>
 #include <FastLED.h>
+
+#define FASTLED_ESP8266_RAW_PIN_ORDER
 
 #define REL_PINA 13
 #define REL_PINB 14
-#define LED_DT 12
+#define LED_PIN 12
 
 // Cambiar por personales
-#define FIREBASE_HOST "https://pythondht11-4ce74-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "5Ir4SU0d8EFs2hZv3imkQszchcJxCDG8v7TzEYpa"
+#define FIREBASE_HOST "https://esp8266-iot-947ea-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "V79839Ek0y82X8rK7tnZMEGlE6fCJNSmTcy2rK9p"
 #define WIFI_SSID "CABLEVISION-eec1"
 #define WIFI_PASSWORD "1805BMISBQHA"
 
@@ -16,15 +19,28 @@
 #define LED_TYPE WS2812
 #define NUM_LEDS 1
 
+#define DHTPIN 15
+#define DHTTYPE DHT11
+
 FirebaseData firebaseData;
 
-uint8_t max_bright = 255;
+DHT dht(DHTPIN, DHTTYPE);
+
+uint8_t max_bright = 100;
 struct CRGB leds[NUM_LEDS];
+
+int estadoLed, inputVal;
+float valorAnterior;
+float temp;
 
 void setup() {
   Serial.begin(115200);
+  dht.begin();
 
-  LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);
+  pinMode(REL_PINA, OUTPUT);
+  pinMode(REL_PINB, OUTPUT);
+
+  LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(max_bright);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -42,25 +58,84 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
-  Firebase.setBool(firebaseData, "led", false);
+  Firebase.setInt(firebaseData, "led", 0);
+  Firebase.setInt(firebaseData, "barra", 0);
 }
 
 void loop() {
-  Firebase.getBool(firebaseData, "led");
-  bool estado = firebaseData.boolData();
-  if (estado == true) {
-    rainbow();
+  if (Firebase.getInt(firebaseData, "led/")) { // Si pongo muchos de estos se bugea mucho
+    estadoLed = firebaseData.intData();
   }
-  else {
-    leds[0] = CRGB::Black;
+
+  delay(10);
+
+  if (estadoLed == 0) {
+    rainbowOff();
+  }
+  else if (estadoLed == 1) {
+    rainbowOn(1, 1);
     FastLED.show();
+  }
+  else if (estadoLed == 2) {
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+  }
+  else if (estadoLed == 3) {
+    fill_solid(leds, NUM_LEDS, CRGB::Green);
+    FastLED.show();
+  }
+  else if (estadoLed == 4) {
+    fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    FastLED.show();
+  }
+  else if (estadoLed == 5) {
+    digitalWrite(REL_PINA, HIGH);
+  }
+  else if (estadoLed == 6) {
+    digitalWrite(REL_PINA, LOW);
+  }
+  else if (estadoLed == 7) {
+    digitalWrite(REL_PINB, HIGH);
+  }
+  else if (estadoLed == 8) {
+    digitalWrite(REL_PINB, LOW);
+  }
+  else if (estadoLed == 9) {
+    poteLed();
+  }
+  // digitalWrite(REL_PINA, LOW);
+  // digitalWrite(REL_PINB, LOW);
+
+  temperature();
+}
+
+void poteLed() {
+  Firebase.getInt(firebaseData, "barra/");
+  inputVal = firebaseData.intData(); // De 0 a 255
+
+  uint8_t hue = inputVal / 4;
+  CHSV color = CHSV(hue, 255, 255);
+  fill_solid(leds, NUM_LEDS, color);
+  FastLED.show();
+}
+
+void temperature() {
+  if (millis() - valorAnterior >= 10000) {
+    valorAnterior = millis();
+    temp = dht.readTemperature();
+    // Serial.println(String(temp) + " " + String(millis()));
+
+    Firebase.setFloat(firebaseData, "Temperatura", temp);
   }
 }
 
-void rainbow() {
-  uint8_t thisSpeed = 10;
-  uint8_t deltaHue = 10;
+void rainbowOn(uint8_t thisSpeed, uint8_t deltaHue) {
+  // uint8_t thisHue = beatsin8(thisSpeed,0,255);
   uint8_t thisHue = beat8(thisSpeed, 255);
   fill_rainbow(leds, NUM_LEDS, thisHue, deltaHue);
+}
+
+void rainbowOff() {
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
   FastLED.show();
 }
